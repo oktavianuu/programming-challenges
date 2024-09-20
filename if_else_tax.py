9e050bcd285459ea56d5ba0933d48dabd1427d65
oktavianuu <oktavianuss.misro@gmail.com>
2024-09-20 14:28:22 +0700

    add if else tax challenge

---
+++ if_else_tax.py
@@ -0,0 +1,34 @@
+"""
+Scenario
+
+Once upon a time there was a land of milk and honey, inhabited by happy and prosperous people. The people 
+paid taxes, of course their happiness had limits. The most important tax, called the Personal Income Tax (PIT for 
+short) had to be paid once a year, and was evaluated using the following rule:
+
+    - if the citizen's income was not higher than 85,528 thalers, the tax was equal to 18% of the income minus 556 
+    thalers and 2 cents (this was what they called tax relief)
+    - if the income was higher than this amount, the tax was equal to 14,839 thalers and 2 cents, plus 32% of the 
+    surplus over 85,528 thalers.
+
+Your task is to write a tax calculator.
+
+    It should accept one floating-point value: the income.
+    Next, it should print the calculated tax, rounded to full thalers. There's a function named round() which will 
+    do the rounding for you. You'll find it in the skeleton code in the editor.
+	
+Note: this happy country never returned any money to its citizens. If the calculated tax was less than zero, 
+it would only mean no tax at all (the tax was equal to zero). Take this into consideration during your calculations.
+"""
+income = float(input("Enter the annual income: "))
+trshold = 85528
+tax = 0
+
+if income < trshold:
+	tax = income * 0.18 - 556.02
+	if tax < 0:
+		tax = 0.0
+elif income > trshold:
+	tax = (14839.02) + (0.32 * (income - 85528)) 
+
+tax = round(tax, 1)
+print("The tax is:", tax, "thalers")
