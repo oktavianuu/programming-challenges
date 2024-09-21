f42a8c247a76d6db10df4cca7150fb60db2398e0
oktavianuu <oktavianuss.misro@gmail.com>
2024-09-22 05:00:11 +0700

    while challenges but not yet finished

---
+++ while_collatz_hyp.py
@@ -0,0 +1,33 @@
+"""
+Scenario
+
+In 1937, a German mathematician named Lothar Collatz formulated an intriguing hypothesis (it still remains unproven) 
+which can be described in the following way:
+
+    take any non-negative and non-zero integer number and name it c0;
+    if it's even, evaluate a new c0 as c0 ÷ 2;
+    otherwise, if it's odd, evaluate a new c0 as 3 × c0 + 1;
+    if c0 ≠ 1, go back to point 2.
+
+The hypothesis says that regardless of the initial value of c0, it will always go to 1.
+
+Of course, it's an extremely complex task to use a computer in order to prove the hypothesis for any natural number 
+(it may even require artificial intelligence), but you can use Python to check some individual numbers. Maybe you'll 
+even find the one which would disprove the hypothesis.
+
+Write a program which reads one natural number and executes the above steps as long as c0 remains different from 1. 
+We also want you to count the steps needed to achieve the goal. Your code should output all the intermediate values 
+of c0, too.
+"""
+c0 = int(input("Enter non-zero and non-negative integer: "))
+
+while c0:
+    if c0 % 2 == 0: # if c0 even
+        c0 = c0 // 2
+    elif c0 != 1: # if c0 != 1
+        c0 = c0 // 2
+    else: # if c0 odd
+        c0 = 3 * c0 + 1
+    print(c0)
+    c0 -= c0
+print("Steps: ", c0)
--- while_pyramid.py
+++ while_pyramid.py
@@ -13,3 +13,11 @@ pyramid that can be built using these blocks.
 Note: the height is measured by the number of fully completed layers - if the builders don't have a sufficient 
 number of blocks and cannot complete the next layer, they finish their work immediately.
 """
+blocks = int(input("Enter the number of blocks: "))
+height = 0
+
+while blocks:
+    height += 1
+    blocks -= height
+
+print("The height of the pyramid:", height)
